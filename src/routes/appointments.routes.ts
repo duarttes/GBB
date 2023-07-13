57467b503334ac16b161d691833ae617c8796e42
Matheus Duarte <maduarttes@gmail.com>
2023-07-13 12:44:12 -0300

    GBB-004-Update endpoint POST appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,35 @@
-import { Router, request } from "express";
+import { Router, request, response } from "express";
 import { uuid } from "uuidv4";
+import { startOfHour, parseISO, isEqual } from "date-fns";
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post("/", (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+  const FindAppointmentInSameDate = appointments.find((appointment) =>
+    isEqual(parsedDate, appointment.date)
+  );
+
+  if (FindAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ msg: "This Appointment is already booked" });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
@@ -19,4 +37,7 @@ appointmentsRouter.post("/", (request, response) => {
   return response.json({ appointment });
 });
 
+appointmentsRouter.get("/", (request, response) => {
+  return response.json(appointments);
+});
 export default appointmentsRouter;
--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log("Decolou 🚀");
+  console.log("TAKE-OFF 🚀");
 });
